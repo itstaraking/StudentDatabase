a4312c1d9e760aee31aa71eff00f554cdc114857
itstaraking <fairgameapps@gmail.com>
2019-07-01 13:58:53 -0400

    Add files via upload

---
+++ StudentForm.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StudentForm.aspx.cs" Inherits="StudentDatabase.StudentForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="height: 67px; width: 790px">
+            <asp:Table ID="Table1" runat="server">
+                <asp:TableRow>
+                    <asp:TableHeaderCell>Student Name</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Start Date</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Program</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Transfer Credits</asp:TableHeaderCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>
+                        <asp:TextBox ID="tbStudentName" runat="server"></asp:TextBox>
+                    </asp:TableCell>
+
+                    <asp:TableCell>
+                        <asp:TextBox ID="tbStartDate" runat="server"></asp:TextBox>
+                    </asp:TableCell>
+
+                    <asp:TableCell>
+                        <asp:TextBox ID="tbProgram" runat="server"></asp:TextBox>
+                    </asp:TableCell>
+
+                    <asp:TableCell>
+                        <asp:TextBox ID="tbTransferCredits" runat="server"></asp:TextBox>
+                    </asp:TableCell>
+
+                    <asp:TableCell>
+                        <asp:Button ID="btnAdd" runat="server" OnClick="Button1_Click" Text="Add" />
+                    </asp:TableCell>
+
+                    <asp:TableCell>
+                        
+                    </asp:TableCell>
+
+                </asp:TableRow>
+            </asp:Table>
+            <asp:Label ID="lblConfirmation" runat="server" Text=" "></asp:Label>
+        </div><br /><br />
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" PageSize="20">
+                <Columns>
+                    <asp:BoundField ItemStyle-Width="150px" DataField="StudentName" HeaderText="Student Name" />
+                    <asp:BoundField ItemStyle-Width="150px" DataField="StartDate" HeaderText="Start Date" />
+                    <asp:BoundField ItemStyle-Width="150px" DataField="Program" HeaderText="Program" />
+                    <asp:BoundField ItemStyle-Width="150px" DataField="TransferCredits" HeaderText="Transfer Credits" />
+                </Columns>
+            </asp:GridView><br />
+            <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" /><br /><br />
+            <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" />
+            <asp:TextBox ID="tbUpdate" runat="server" placeholder="Enter Name to Update" ></asp:TextBox>
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ StudentForm.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace StudentDatabase
+{
+    public partial class StudentForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                this.BindGrid();
+
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO Students(StudentName, StartDate, Program, TransferCredits) VALUES(@StudentName, @StartDate, @Program, @TransferCredits)";
+            if(tbStudentName.Text !="" && tbStartDate.Text !="" && tbProgram.Text != "")
+            {
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString))
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    command.Parameters.AddWithValue("@StudentName", tbStudentName.Text);
+                    command.Parameters.AddWithValue("@StartDate", tbStartDate.Text);
+                    command.Parameters.AddWithValue("@Program", tbProgram.Text);
+                    command.Parameters.AddWithValue("@TransferCredits", tbTransferCredits.Text);
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    ClearFields();
+                }
+            }
+            else
+            {
+                lblConfirmation.Text = "Please Enter Student Name, Start Date and Program";
+            }
+            BindGrid();
+        }
+        private void ClearFields()
+        {
+            tbStudentName.Text = "";
+            tbStartDate.Text = "";
+            tbProgram.Text = "";
+            tbTransferCredits.Text = "";
+            tbStudentName.Focus();
+        }
+
+        private void BindGrid()
+        {
+            string connection = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connection))
+            {
+                using (SqlCommand command = new SqlCommand("SELECT StudentName, StartDate, Program, TransferCredits FROM Students"))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        command.Connection = conn;
+                        sda.SelectCommand = command;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            GridView1.DataSource = dt;
+                            GridView1.DataBind();
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(tbStudentName.Text != "")
+            {
+                string query = "DELETE FROM Students WHERE StudentName = @StudentName";
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString))
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    command.Parameters.AddWithValue("@StudentName", tbStudentName.Text);
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    ClearFields();
+                    BindGrid();
+                }
+            }
+            else
+            {
+                lblConfirmation.Text = "Enter First Name To Be Deleted";
+                ClearFields();
+            }
+
+
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string query = "UPDATE Students SET StudentName = @StudentName, StartDate = @StartDate, Program = @Program, TransferCredits = @TransferCredits WHERE StudentName = @StudentName";
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                command.Parameters.AddWithValue("@StudentName", tbStudentName.Text);
+                command.Parameters.AddWithValue("@StartDate", tbStartDate.Text);
+                command.Parameters.AddWithValue("@Program", tbProgram.Text);
+                command.Parameters.AddWithValue("@TransferCredits", tbTransferCredits.Text);
+                command.ExecuteNonQuery();
+                connection.Close();
+                ClearFields();
+                BindGrid();
+            }
+        }
+    }
+}
